b1f499b39fcaea6d160f8ba9e5becfa3b9ccba3c
Your Name <you@example.com>
2024-08-11 23:40:20 -0700

    add the content of airbnb_clone_v3

---
+++ api/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+creat a flask app blueprint
+"""
+from flask import blueprints
+
+app_views = Blueprints('app_views', __name__, url_prefix'/api/v1')
+
+from api.v1.views.index import*
